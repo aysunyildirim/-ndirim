1751f58ad5df1eaad9ceceee1beafdd02b72168b
Aysun YILDIRIM <82328345+aysunyildirim@users.noreply.github.com>
2023-05-29 12:37:45 +0300

    Add project files.

---
+++ İndirim/Class1.cs
@@ -0,0 +1,19 @@
+﻿namespace İndirim
+{
+    internal class İndirim
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("Kac lira alisveris yaptın");
+            var can = Console.ReadLine();
+            int yeni = Convert.ToInt32();
+
+            yeni = yeni/10;
+
+            Console.WriteLine(yeni +"tl indirim");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ İndirim/Program.cs
@@ -0,0 +1,19 @@
+﻿
+namespace İndirim
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Kac lira alısveris yaptın?");
+            var can = Console.ReadLine();
+            int yeni = Convert.ToInt32(Console.ReadLine(can);
+            yeni = yeni / 10;
+            Console.WriteLine(yeni + "tl indirim kazandın.");
+            Console.ReadLine();
+
+        }
+    }
+}
